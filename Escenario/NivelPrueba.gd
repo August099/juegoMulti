1bef469f417193fb594e190372123cf1f0fb47b8
Andres Roncaglia <163073522+andres-roncaglia@users.noreply.github.com>
2025-12-30 16:42:49 -0300

    Players cant join if the server is not in lobby

--- Escenario/NivelPrueba.gd
+++ Escenario/NivelPrueba.gd
@@ -1,21 +1,30 @@
 extends Node2D
 
+@onready var multiplayer_node = get_node("/root/Multiplayer")
+@onready var game_state = multiplayer_node.game_state
+
+enum GameState{MENU, LOBBY, IN_GAME}
 
 func _ready():
 	# We only need to spawn players on the server.
 	if not multiplayer.is_server():
 		return
-
+	
 	multiplayer.peer_connected.connect(add_player)
 	multiplayer.peer_disconnected.connect(del_player)
 
 	# Spawn already connected players
 	for id in multiplayer.get_peers():
 		add_player(id)
-
+		
 	# Spawn the local player unless this is a dedicated server export.
 	if not OS.has_feature("dedicated_server"):
 		add_player(1)
+		
+	multiplayer_node.game_state = GameState.IN_GAME
+	game_state = multiplayer_node.game_state
+	
+	print("changed gamestate from level:", game_state)
 
 
 func _exit_tree():
@@ -30,7 +39,10 @@ func add_player(id: int):
 	# Set player id.
 	character.player = id
 	character.name = str(id)
-	$Players.add_child(character, true)
+	
+	print("player id added to match: ", id, "current game state: ", game_state)
+	if game_state == GameState.LOBBY:
+		$Players.add_child(character, true)
 
 
 func del_player(id: int):
